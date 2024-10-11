d54b73011689d3e6096adf7efc97ba2c0268f49d
Emre Özel <ozllemree@gmail.com>
2024-10-11 15:18:46 +0300

    DevOps Pipeline

---
+++ src/main/java/com/emreozel/AppMainPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.emreozel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMainPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMainPipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emreozel/AppMainPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emreozel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMainPipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
